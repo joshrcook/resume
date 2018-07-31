d7551c465f268eb34a6d77570fef5c113799e72e
Joshua <jcook@redventures.com>
2018-07-31 02:35:04 -0700

    Finished up for now

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,83 @@
-import { scrollspy, util } from 'uikit';
+import UIkit from 'uikit';
 
-const bodyEl = document.querySelector('body');
+// Add functionality for color changes
+(function() {
+  const bodyEl = document.querySelector('body');
 
-function getBodyClass(el) {
-  return el.dataset.bodyClass;
-}
+  // set the original body class
+  bodyEl.classList.add('color-scheme');
 
-scrollspy('.uk-section', {
-  offsetTop: window.innerHeight / 2 * -1,
-  repeat: true,
-  hidden: false,
-});
+  function getBodyClass(el) {
+    return el.dataset.bodyClass;
+  }
 
-// set the original body class
-bodyEl.classList.add('color-scheme');
-
-util.on('.uk-section', 'inview', (e) => {
-  const bodyClass = getBodyClass(e.target);
-  if( typeof bodyClass === 'undefined' ) return;
-  bodyEl.classList.add(bodyClass);
-});
-
-util.on('.uk-section', 'outview', (e) => {
-  const bodyClass = getBodyClass(e.target);
-  if( typeof bodyClass === 'undefined' ) return;
-  bodyEl.classList.remove(bodyClass);
-});
-
-let liDelay = 250;
-document.querySelectorAll('.uk-subnav > li').forEach((el) => {
-  scrollspy(el, {
-    cls: 'uk-animation-fade',
-    delay: liDelay,
+  UIkit.scrollspy('.uk-section', {
+    offsetTop: window.innerHeight / 2 * -1,
+    repeat: true,
+    hidden: false,
   });
-  liDelay += 250;
-});
+  
+  UIkit.util.on('.uk-section', 'inview', (e) => {
+    const bodyClass = getBodyClass(e.target);
+    if( typeof bodyClass === 'undefined' ) return;
+    bodyEl.classList.add(bodyClass);
+  });
+  
+  UIkit.util.on('.uk-section', 'outview', (e) => {
+    const bodyClass = getBodyClass(e.target);
+    if( typeof bodyClass === 'undefined' ) return;
+    bodyEl.classList.remove(bodyClass);
+  });
+}());
 
-scrollspy('.uk-h2, .uk-h2 + *', {
-  cls: 'uk-animation-slide-bottom-medium',
-  offsetTop: window.innerHeight / -4,
-});
+
+// Add animation functions
+(function() {
+  let liDelay = 500;
+
+  document.querySelectorAll('.uk-subnav > li').forEach((el) => {
+    UIkit.scrollspy(el, {
+      cls: 'uk-animation-fade',
+      delay: liDelay,
+    });
+    liDelay += 250;
+  });
+
+  UIkit.scrollspy('.uk-h1', {
+    cls: 'uk-animation-fade uk-animation-slow',
+    hidden: true,
+    delay: 250,
+  });
+
+  UIkit.scrollspy('.uk-h2', {
+    cls: 'uk-animation-slide-bottom-medium',
+    offsetTop: window.innerHeight / -4,
+  });
+
+  document.querySelectorAll('.uk-h2').forEach((el) => {
+    function addAnimRecursive(el) {
+      UIkit.scrollspy(el, {
+        cls: 'uk-animation-slide-bottom-medium', 
+        offsetTop: window.innerHeight / -4,
+      });
+      // console.log(el.nextElementSibling);
+      if (el.nextElementSibling !== null) {
+        addAnimRecursive(el.nextElementSibling);
+      }
+    }
+    addAnimRecursive(el.nextElementSibling);
+  });
+}());
+
+
+(function() {
+  // Set all sections to be at least the height of the viewport
+  UIkit.heightViewport('.uk-section');
+}());
+
+
+(function() {
+  // set the subnav li's to autoscroll to their respective links
+  UIkit.scroll('.js-subnav a');
+}());
+
